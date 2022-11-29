fd0ac0ad2ee3e7f1ab05a39c907be46a74bccba2
STIMUSHKA <82060916+STIMUSHKA@users.noreply.github.com>
2022-11-30 01:51:23 +0700

    COMMIT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
+
+import { MatTab } from '@angular/material/tabs';
+import { MatTabGroup } from '@angular/material/tabs';
+import { VjsPlayer } from './vjs-player/vjs-player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    VjsPlayer,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
