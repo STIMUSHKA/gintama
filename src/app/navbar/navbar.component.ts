b8f3ffaef03ed201f8084e7cce95f48ad2855f17
STIMUSHKA <82060916+STIMUSHKA@users.noreply.github.com>
2022-11-30 06:15:41 +0700

    added season itteration

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatTab } from '@angular/material/tabs';
 import { MatTabGroup } from '@angular/material/tabs';
+import { ApiService } from '../api-service.service'
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  public seasonsData: any;
 
+  constructor(
+    private _apiService: ApiService
+  ) {}
+
+  ngOnInit() {
+    this.seasonsData = this._apiService.getSeasonsInfo()
+  }
 }
--- src/app/season/season.component.ts
+++ src/app/season/season.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Season } from '../interfaces';
 @Component({
   selector: 'app-season',
   templateUrl: './season.component.html',
   styleUrls: ['./season.component.scss']
 })
 export class SeasonComponent {
+
+  @Input() season: Season;
 
 }
